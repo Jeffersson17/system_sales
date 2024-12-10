2e19971bebe9dadc401a6885cee71918ced9fd91
Jeffersson <barrosjefferson407@gmail.com>
2024-12-10 18:57:05 -0300

    Algumas mudanças nas views

--- django/addresses/views.py
+++ django/addresses/views.py
@@ -1,25 +1,25 @@
 from addresses.models import Address, City
 from addresses.serializers import AddressSerializer, CitySerializer
-from rest_framework import viewsets, generics
+from rest_framework import generics, viewsets
 
 
 class AddressViewSet(viewsets.ModelViewSet):
-    queryset = Address.objects.all().order_by('id')
+    queryset = Address.objects.all().order_by("id")
     serializer_class = AddressSerializer
 
 
 class CityViewSet(viewsets.ModelViewSet):
-    queryset = City.objects.all().order_by('id')
+    queryset = City.objects.all().order_by("id")
     serializer_class = CitySerializer
 
 
 class AddressListAPIView(generics.ListAPIView):
-    queryset = Address.objects.all().order_by('id')
+    queryset = Address.objects.all().order_by("id")
     serializer_class = AddressSerializer
 
 
 class CityListAPIView(generics.ListAPIView):
-    queryset = City.objects.all().order_by('id')
+    queryset = City.objects.all().order_by("id")
     serializer_class = CitySerializer
 
 
